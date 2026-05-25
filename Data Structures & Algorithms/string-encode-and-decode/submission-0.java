8d7cbb76a38a4c2686a62d9101947fcac5f0066f
subash016-stack <subash16.01.2006@gmail.com>
2026-05-25 13:30:16 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public String encode(List<String> strs) 
+    {
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs)
+        {
+            sb.append(str.length()).append("#").append(str);
+
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) 
+    {
+        List<String> arr = new ArrayList<>();
+        int i=0;
+        while(i<str.length())
+        {
+            int j=i;
+            while(str.charAt(j)!='#')
+            {
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i,j));
+            String word = str.substring(j+1,j+1+length);
+            arr.add(word);
+            i=j+1+length;
+        }
+        return arr;
+    }
+}
