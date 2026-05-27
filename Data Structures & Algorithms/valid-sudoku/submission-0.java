c183e20ee3742d50c1b9936262660bffef92d0a8
subash016-stack <subash16.01.2006@gmail.com>
2026-05-27 16:27:27 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public boolean isValidSudoku(char[][] board) 
+    {
+        HashSet<String> set = new HashSet<>();
+        for(int r=0;r<9;r++)
+        {
+            for(int c=0;c<9;c++)
+            {
+                char num = board[r][c];
+                if(num=='.')
+                {
+                    continue;
+                }
+                String row="row"+r+num;
+                String col="col"+c+num;
+                String box="box"+(r/3)+(c/3)+num;
+                if(set.contains(row)||set.contains(col)||set.contains(box))
+                {
+                    return false;
+                }
+                set.add(row);
+                set.add(col);
+                set.add(box);
+            }
+        }
+        return true;
+    }
+    
+}
