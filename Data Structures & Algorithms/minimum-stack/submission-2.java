dc1a9f5cb3e3eca96ce6dcfefa9a301b2a966f9a
subash016-stack <subash16.01.2006@gmail.com>
2026-06-14 21:16:54 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,39 @@
+class MinStack {
+    Stack<Integer>stack;
+    Stack<Integer>minstack;
+    public MinStack() 
+    {
+        stack=new Stack<>();
+        minstack=new Stack<>();
+    }
+    
+    public void push(int val) 
+    {
+        stack.push(val);
+        if(minstack.isEmpty())
+        {
+            minstack.push(val);
+        }
+        else
+        {
+            int min=Math.min(val,minstack.peek());
+            minstack.push(min);
+        }
+    }
+    
+    public void pop() 
+    {
+        stack.pop();
+        minstack.pop();
+    }
+    
+    public int top() 
+    {
+       return stack.peek();
+    }
+    
+    public int getMin() 
+    {
+       return minstack.peek();        
+    }
+}
