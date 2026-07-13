69612f0bcd0678865f54f72bfbd70da4af7a2dde
subash016-stack <subash16.01.2006@gmail.com>
2026-07-13 11:06:41 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        int left=0;
+        int right=1;
+        int maxprofit=0;
+        while(right<prices.length)
+        {
+            if(prices[left]<prices[right])
+            {
+                int profit = prices[right]-prices[left];
+                maxprofit=Math.max(profit,maxprofit);
+            }
+            else
+            {
+                left=right;
+            }
+            right++;
+        }
+        return maxprofit;
+    }
+}
