a4973725c644f39782f35c068e1aa8d5ef448bc7
subash016-stack <subash16.01.2006@gmail.com>
2026-07-16 23:01:57 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int findDuplicate(int[] nums) 
+    {
+        int fast = nums[0];
+        int slow = nums[0];
+        do
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(fast!=slow);
+        fast=nums[0];
+        while(fast!=slow)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+} 
