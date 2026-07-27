a1773a34e16f414ae0110be1ad33fd23d018f7a3
subash016-stack <subash16.01.2006@gmail.com>
2026-07-27 22:18:41 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) 
+    {
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int num : stones)
+        {
+            pq.offer(num);
+        }
+        while(pq.size()>1)
+        {
+            int first = pq.poll();
+            int second = pq.poll();
+            pq.offer(first-second); 
+        }
+        if(pq.isEmpty())
+        {
+            return 0;
+        }
+        return pq.peek();
+    }
+}
