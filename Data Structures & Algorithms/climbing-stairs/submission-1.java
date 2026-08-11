b7ebaa39a8ffe1dfb8532a2bdbbc8daf3aeb1b16
subash016-stack <subash16.01.2006@gmail.com>
2026-08-11 22:22:27 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int climbStairs(int n) 
+    {
+        if(n<=2)
+            return n;
+            
+        int one=1;
+        int two=2;
+        for(int i=3;i<=n;i++)
+        {
+            int current = two+one;
+            one=two;
+            two=current;
+        }
+        return two;
+    }
+}
