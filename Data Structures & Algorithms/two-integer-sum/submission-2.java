9d9734c96778d8b81e510b9111ac6fa49103c467
subash016-stack <subash16.01.2006@gmail.com>
2026-09-04 18:42:44 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target)
+    {
+        int left=0;
+        int right=nums.length-1;
+        int[] arr = new int[2];
+        while(left<right)
+        {
+            int sum=nums[left]+nums[right];
+            if(sum==target)
+            {
+                return new int[]{left,right};
+            }
+            else if(sum<target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return new int[2];
+    }
+}
