393cba98aaef19a76c90354f4a3f0462137c2f21
subash016-stack <subash16.01.2006@gmail.com>
2026-09-06 09:42:16 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int carFleet(int target, int[] position, int[] speed) 
+    {
+        int n = position.length;
+        Integer[]order = new Integer[n];
+        for(int i=0;i<n;i++)
+        {
+            order[i]=i;
+        }
+        Arrays.sort(order,(a,b)->position[b]-position[a]);
+        Stack<Double> stack = new Stack<>();
+        for(int i : order)
+        {
+            double time = (target-position[i])/speed[i];
+            if(stack.isEmpty() || time>stack.peek())
+            {
+                stack.push(time);
+            }
+        }
+       return stack.size();
+    }
+}
